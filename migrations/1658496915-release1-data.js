89e7a767e4d9d4fca77606f348a853f27e89eecb
anitmatter65 <dominicowenmoss@gmail.com>
2022-07-22 16:56:43 +0200

    ommented out release1-data migration due to problems deploying

--- migrations/1658496915-release1-data.js
+++ migrations/1658496915-release1-data.js
@@ -1,19 +1,19 @@
-exports.up = async (sql) => {
-  await sql`
-  INSERT INTO release1
-  (track_number, track_name, contributing_artists, release_id, track_length )
-  VALUES
-   (1, 'Closer', 'Tricia McTeague', 1, '5:08'),
-   (2, 'Cloer (Instrumental)', '', 1, '5:08'),
-   (1, 'White Label 1 - TEST DATA', 'TEST DATA COLAB', 2, '5:00'),
-   (2, 'White Label 1 (Istrumental)', '', 2, '5:00' ),
-   (1, 'White Label 2 - TEST DATA', 'TEST DATA COLAB', 3, '5:00'),
-   (2, 'White Label 2 - TEST DATA (Istrumental)', '', 3, '5:00' )
-`;
-};
+// exports.up = async (sql) => {
+//   await sql`
+//   INSERT INTO release1
+//   (track_number, track_name, contributing_artists, release_id, track_length )
+//   VALUES
+//    (1, 'Closer', 'Tricia McTeague', 1, '5:08'),
+//    (2, 'Cloer (Instrumental)', '', 1, '5:08'),
+//    (1, 'White Label 1 - TEST DATA', 'TEST DATA COLAB', 2, '5:00'),
+//    (2, 'White Label 1 (Istrumental)', '', 2, '5:00' ),
+//    (1, 'White Label 2 - TEST DATA', 'TEST DATA COLAB', 3, '5:00'),
+//    (2, 'White Label 2 - TEST DATA (Istrumental)', '', 3, '5:00' )
+// `;
+// };
 
-exports.down = async (sql) => {
-  await sql`
-	DELETE FROM release1
-	`;
-};
+// exports.down = async (sql) => {
+//   await sql`
+// 	DELETE FROM release1
+// 	`;
+// };
